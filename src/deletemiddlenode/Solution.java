a7308c59a3dc26a43f31f279d40ff9f263978b0c
SridharVadla <vadlasreedhar123@gmail.com>
2025-01-26 12:40:05 +0530

    Solved Problem 10: delete node from middle of the linkedlist

---
+++ src/deletemiddlenode/Solution.java
@@ -0,0 +1,19 @@
+package src.deletemiddlenode;
+import src.binaryLinkedListToInteger.Solution.*;
+public class Solution {
+
+    public ListNode deleteMiddle(ListNode head) {
+        if(head==null || head.next =null) return null;
+        ListNode slow =head;
+        ListNode fast =head;
+        ListNode prev = null;
+
+        while(fast!=null && fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=slow.next;
+        return head;
+    }
+}
